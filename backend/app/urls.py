94cfcaf54c1ba87199e283e51afeaf8f0b1c14d1
Kacper Baum <Kaba8081@gmail.com>
2024-12-12 18:38:34 +0100

    Feat. Simple frontend api, django view improvements

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt import views as jwt_views
 
 from .views import UserViewSet
 from .views import ProfileViewSet
@@ -10,5 +11,7 @@ router.register(r'profile', ProfileViewSet, basename='profile')
 
 
 urlpatterns = [
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
