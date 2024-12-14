6230d1160aa5f68bfa381043e7bb33614dea69de
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-14 14:16:06 +0100

    Added Sidenav inheritanc, made it wider

--- frontend/app/(mainapp)/aktywnosc/layout.tsx
+++ frontend/app/(mainapp)/aktywnosc/layout.tsx
@@ -3,7 +3,6 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
 import localFont from "next/font/local";
-import SideNav from "@/app/ui/sidenav";
 import SideNavEvents from "@/app/ui/sidenav-events";
 
 // const geistSans = localFont({
@@ -34,7 +33,7 @@ export default function Layout({
   return  (
     <span lang='en' suppressHydrationWarning>
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
+      <div className="w-full flex-none md:w-72">
         <SideNavEvents />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-10">{children}</div>
--- frontend/app/ui/sidenav-main.tsx
+++ frontend/app/ui/sidenav-main.tsx
@@ -5,13 +5,10 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 import SideNav from './sidenav';
 //import { signOut } from '@/auth';
 
-export default function SideNavEvents() {
+export default function SideNavMain() {
   return (
-    
     <SideNav>
-      <div className="flex h-full flex-col px-3 py-4 md:px-2">
-        abcde
-      </div>
+      <NavLinks></NavLinks>
     </SideNav>
   );
 }
