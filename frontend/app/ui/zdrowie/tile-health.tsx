1a100846c24386aefb6ca740d2fad5c533af5ec3
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-14 19:39:09 +0100

    refactor: Improve layout and styling in health tiles component

--- frontend/app/ui/zdrowie/tile-health.tsx
+++ frontend/app/ui/zdrowie/tile-health.tsx
@@ -1,42 +1,25 @@
-import clsx from "clsx";
-import Image from "next/image";
-import { MapIcon } from "@heroicons/react/24/outline";
+
 
 interface TileData {
-    title: string;
-    text: string;
+  title: string;
+  text: string;
+}
 
-  }
-  
-export default function TileHealth({ tileData }: {tileData: TileData}) {
-
+export default function TileHealth({ tileData }: { tileData: TileData }) {
   return (
-    <div className="flex flex-col border w-full text-xl ">
-      <div
-        className="flex flex-row items-center justify-between px-7 py-4 w-full"
-      >
-        <div className="flex items-center">
-
-          <div className="min-w-0 mr-2">
-            <p className="truncate font-semibold ">
-              {tileData.title}
-            </p>
-            <p className="hidden text-sm text-gray-500 sm:block">
-              {tileData.text}
-            </p>
-          </div>
-
+    <div className="flex flex-col border w-full bg-amber-50">
+      <div className="flex flex-row items-center justify-between px-7 py-4 w-full text-3xl">
+        <div className="min-w-0 mr-2">
+          <p className="truncate font-semibold ">{tileData.title}</p>
+          {/* <p className="hidden text-sm text-gray-500 sm:block">
+            {tileData.text}
+          </p> */}
         </div>
-
       </div>
 
       <hr></hr>
-      <div className="flex flex-col w-full px-7 py-4">
-        
-        <p className="pl-1 pt-2">
-            {tileData.text}
-        </p>
-
+      <div className="flex flex-col w-full px-7 py-4 text-2xl">
+        <p className="pl-1 pt-2">{tileData.text}</p>
       </div>
     </div>
   );
--- frontend/app/ui/zdrowie/tiles-health.tsx
+++ frontend/app/ui/zdrowie/tiles-health.tsx
@@ -2,7 +2,7 @@ import clsx from "clsx";
 import Image from "next/image";
 import { MapIcon } from "@heroicons/react/24/outline";
 import TileHealth from "./tile-health";
-
+import imieninyData from "@/public/zdrowie/imieniny.json";
 
 export default function TilesHealth() {
 
@@ -13,10 +13,23 @@ export default function TilesHealth() {
         "trivia": "Na noworocznych obyczajach w Szkocji spożywa się haggis.",
         "joke": "Dlaczego komputer się przeziębił? Bo zostawił otwarte okno."}
 
-  return (
+  const weather = "Zachmurzenie umiarkowane, temperatura 10°C, wiatr 15 km/h"; // TODO: fetch from API
+
+  const today = new Date();
+  const formattedDate = `${String(today.getMonth() + 1).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
+  let todayNameDay = imieninyData.find((item) => item.date === formattedDate);
+
+  const daysOfWeek = ["Niedziela", "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota"];
+  const dayOfWeek = daysOfWeek[today.getDay()];
+  if(!todayNameDay)
+    todayNameDay={ date: '12-14', name_day: 'Alfreda, Izydora (placeholder)' }
+  console.log(todayNameDay);
+
+
+  return (  
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      <TileHealth tileData={{ title: "Data", text: tilesData.date }} />
-      <TileHealth tileData={{ title: "Imieniny", text: tilesData.name_day }} />
+      <TileHealth tileData={{ title: `${dayOfWeek} ${formattedDate}`, text: `Imieniny obchodzą dzisiaj: ${todayNameDay.name_day}`}} />
+      <TileHealth tileData={{ title: "Pogoda", text: weather }} />  
       <TileHealth tileData={{ title: "Porada zdrowotna", text: tilesData.health_advise }} />
       <TileHealth tileData={{ title: "Zdrowy posiłek na dziś", text: tilesData.healthy_meal }} />
       <TileHealth tileData={{ title: "Ciekawostka", text: tilesData.trivia }} />
