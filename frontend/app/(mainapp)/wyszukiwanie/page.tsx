29935af1bf41177f2db5ec946e9c4916e7af8aed
MrNtex <rtrniemiec@gmail.com>
2024-12-15 00:38:20 +0100

    style: Improved the readibility of search page

--- frontend/app/(mainapp)/wyszukiwanie/page.tsx
+++ frontend/app/(mainapp)/wyszukiwanie/page.tsx
@@ -3,13 +3,17 @@
 import SearchResult from '@/app/ui/search/SearchItem';
 import { Input } from '@/components/ui/input';
 import { FetchDomains, SearchItem } from '@/services/Search';
+import { HeartCrack, Search } from 'lucide-react';
 import React, { useState } from 'react';
 
 export default function Page() {
-  const [searchItems, setSearchItems] = useState<SearchItem[]>([]); // Initialize with an empty array
+  const [searchItems, setSearchItems] = useState<SearchItem[]>([]);
+  const [charCount, setCharCount] = useState(0); // State to track the number of characters
 
   async function SearchItem(event: React.ChangeEvent<HTMLInputElement>) {
     const query = event.target.value.trim();
+    setCharCount(query.length); // Update the character count
+
     if (query === '') {
       setSearchItems([]); // Clear results if the query is empty
       return;
@@ -17,7 +21,6 @@ export default function Page() {
 
     try {
       const results = FetchDomains(query);
-      console.log("Fetching...")
       setSearchItems(results);
     } catch (error) {
       console.error("Error fetching search items:", error);
@@ -25,7 +28,7 @@ export default function Page() {
     }
   }
 
-  const showResults = searchItems.length > 0
+  const showResults = searchItems.length > 0;
 
   return (
     <div 
@@ -34,19 +37,35 @@ export default function Page() {
       <div className={`w-full flex flex-col transition-all duration-500 ease-in-out ${
         showResults ? 'mt-8' : 'mt-[25%]'
       }`}>
-        <span className='text-center justify-center text-4xl mb-8 font-extrabold'>Wyszukaj</span>
+        <div className='flex items-center justify-center'>
+          <h1 className='p-4 flex gap-2 items-center text-white text-4xl mb-8 font-extrabold bg-amber-600 border-white rounded-full shadow-lg drop-shadow-md'>
+            Wyszukaj
+            <Search size={32}/>
+          </h1>
+        </div>
+        
         <Input 
             onInput={SearchItem} 
-            className='lg:2text-xl md:text-2xl sm:text-lg w-full h-12 px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
+            className='lg:2text-xl md:text-2xl sm:text-lg w-full h-12 px-3 py-2 border-4 border-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500'
         />
       </div>
-      {showResults &&
-        <div className='search-results mt-6'>
-          {searchItems.map((item, index) => (
-            <SearchResult item={item} key={index}/>
-          ))}
-        </div>
+      {
+        charCount > 0 &&
+        (showResults ? (
+          <div className='search-results mt-6'>
+            {searchItems.map((item, index) => (
+              <SearchResult item={item} key={index}/>
+            ))}
+          </div>) : (
+            <div className='search-results mt-6 text-red-600 flex gap-2'>
+            <HeartCrack /> 
+            Niestety, nie mamy tego czego szukasz
+            
+          </div>
+          )
+        )
       }
+      
     </div>
   );
 }
