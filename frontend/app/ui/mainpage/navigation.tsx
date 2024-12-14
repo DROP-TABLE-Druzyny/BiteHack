9c9839309302ab7d74e67411beb99040877c3b97
MrNtex <rtrniemiec@gmail.com>
2024-12-15 00:49:48 +0100

    feat: Improved calculating angle in the main page

--- frontend/app/ui/mainpage/navigation.tsx
+++ frontend/app/ui/mainpage/navigation.tsx
@@ -3,28 +3,29 @@
 import React, { useEffect, useState } from 'react';
 import { InformationCircleIcon, UserIcon, MapIcon, UserGroupIcon, HeartIcon } from '@heroicons/react/24/outline';
 import NavItem from './navitem';
+import { Search } from 'lucide-react';
 
 export interface NavItemProps {
   href: string;
   icon: React.FC<React.SVGProps<SVGSVGElement>>;
   label: string;
   radius: number;
-  angle: number; // Angle in degrees
   style?: React.CSSProperties
 }
 
 const Navigation: React.FC = () => {
-  const dist = 60;
+  const dist_main = 60;
+  const dist_far = 120;
 
   const [items, setItems] = useState<NavItemProps[]>([
-    { href: '/zdrowie', icon: HeartIcon, label: 'Zdrowie', radius: 60, angle: 0 },
-    { href: '/aktywnosc', icon: MapIcon, label: 'Wydarzenia', radius: 60, angle: 72 },
-    { href: '/pomocnik', icon: UserGroupIcon, label: 'Pomocnik', radius: 60, angle: 144 },
-    { href: '/profil', icon: UserIcon, label: 'Profil', radius: 60, angle: 216 },
-    { href: '/informacje', icon: InformationCircleIcon, label: 'Informacje', radius: 60, angle: 288 },
+    { href: '/zdrowie', icon: HeartIcon, label: 'Zdrowie', radius: 60},
+    { href: '/aktywnosc', icon: MapIcon, label: 'Wydarzenia', radius: 60},
+    { href: '/pomocnik', icon: UserGroupIcon, label: 'Pomocnik', radius: 60 },
+    { href: '/wyszukiwanie', icon: Search, label: 'Wyszukiwarka', radius: 60},
+    { href: '/informacje', icon: InformationCircleIcon, label: 'Informacje', radius: 60 },
   ]);
 
-  const getPosition = (radius: number, angle: number) => {
+  const getPosition = (radius: number, angle: number, dist: number) => {
     const rad = (Math.PI / 180) * angle;
     return {
       x: Math.cos(rad) * (radius + dist),
@@ -32,27 +33,39 @@ const Navigation: React.FC = () => {
     };
   };
 
+  const NavItems = () => 
+  {
+    let navItems = []
+
+    const n = items.length;
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      const { x, y } = getPosition(item.radius, (360/n)*i, dist_main);
+      
+      navItems.push(
+        <NavItem
+          key={item.href}
+          href={item.href}
+          icon={item.icon}
+          label={item.label}
+          radius={item.radius}
+          style={{
+            position: 'absolute',
+            left: `calc(50% + ${x}px)`,
+            top: `calc(50% + ${y}px)`,
+            transform: 'translate(-50%, -50%)',
+          }}
+        />
+      );
+    }
+
+    return navItems;
+  }
+
   return (
     <div className="relative flex justify-center items-center w-80 md:w-96 h-80 md:h-96">
-      {items.map((item) => {
-        const { x, y } = getPosition(item.radius, item.angle);
-        return (
-          <NavItem
-            key={item.href}
-            href={item.href}
-            icon={item.icon}
-            label={item.label}
-            radius={item.radius}
-            angle={item.angle}
-            style={{
-              position: 'absolute',
-              left: `calc(50% + ${x}px)`,
-              top: `calc(50% + ${y}px)`,
-              transform: 'translate(-50%, -50%)',
-            }}
-          />
-        );
-      })}
+      <NavItems/>
     </div>
   );
 };
