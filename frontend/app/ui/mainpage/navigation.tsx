3f87488dcfef46f1cfa0f6e15388c976ab264a17
MrNtex <rtrniemiec@gmail.com>
2024-12-14 20:40:45 +0100

    feat: Improve hero page

--- frontend/app/ui/mainpage/navigation.tsx
+++ frontend/app/ui/mainpage/navigation.tsx
@@ -1,15 +1,58 @@
-import React from 'react';
+'use client'
+
+import React, { useEffect, useState } from 'react';
 import { InformationCircleIcon, UserIcon, MapIcon, UserGroupIcon, HeartIcon } from '@heroicons/react/24/outline';
 import NavItem from './navitem';
 
+export interface NavItemProps {
+  href: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+  label: string;
+  radius: number;
+  angle: number; // Angle in degrees
+  style?: React.CSSProperties
+}
+
 const Navigation: React.FC = () => {
+  const dist = 60;
+
+  const [items, setItems] = useState<NavItemProps[]>([
+    { href: '/zdrowie', icon: HeartIcon, label: 'Zdrowie', radius: 60, angle: 0 },
+    { href: '/activity', icon: MapIcon, label: 'Wydarzenia', radius: 60, angle: 72 },
+    { href: '/pomocnik', icon: UserGroupIcon, label: 'Pomocnik', radius: 60, angle: 144 },
+    { href: '/profil', icon: UserIcon, label: 'Profil', radius: 60, angle: 216 },
+    { href: '/informacje', icon: InformationCircleIcon, label: 'Informacje', radius: 60, angle: 288 },
+  ]);
+
+  const getPosition = (radius: number, angle: number) => {
+    const rad = (Math.PI / 180) * angle;
+    return {
+      x: Math.cos(rad) * (radius + dist),
+      y: Math.sin(rad) * (radius + dist),
+    };
+  };
+
   return (
     <div className="relative flex justify-center items-center w-80 md:w-96 h-80 md:h-96">
-      <NavItem href="/zdrowie" icon={HeartIcon} label="Zdrowie" position="top-4 left-4" />
-      <NavItem href="/activity" icon={MapIcon} label="Wydarzenia" position="top-4 right-4" />
-      <NavItem href="/pomocnik" icon={UserGroupIcon} label="Pomocnik" position="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" isMain={true} />
-      <NavItem href="/profil" icon={UserIcon} label="Profil" position="bottom-4 left-4" />
-      <NavItem href="/informacje" icon={InformationCircleIcon} label="Informacje" position="bottom-4 right-4" />
+      {items.map((item) => {
+        const { x, y } = getPosition(item.radius, item.angle);
+        return (
+          <NavItem
+            key={item.href}
+            href={item.href}
+            icon={item.icon}
+            label={item.label}
+            radius={item.radius}
+            angle={item.angle}
+            style={{
+              position: 'absolute',
+              left: `calc(50% + ${x}px)`,
+              top: `calc(50% + ${y}px)`,
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+        );
+      })}
     </div>
   );
 };
--- frontend/app/ui/mainpage/navitem.tsx
+++ frontend/app/ui/mainpage/navitem.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { NavItemProps } from './navigation';
 
-interface NavItemProps {
-  href: string;
-  icon: React.ElementType;
-  label: string;
-  position: string;
-  isMain?: boolean
-}
 
-const NavItem: React.FC<NavItemProps> = ({ href, icon: Icon, label, position, isMain = false }) => (
-  <a href={href} className={`absolute flex flex-col items-center justify-center ${isMain ? "w-36 h-36 md:w-40 md:h-40" : "w-24 h-24 md:w-32 md:h-32"} bg-amber-600 border-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 ${position}`}>
+const NavItem: React.FC<NavItemProps> = ({ href, icon: Icon, label, radius, style}) => (
+  <a
+    href={href}
+    style={{
+      ...style,
+      width: `${radius * 2}px`, // Double the radius for width and height
+      height: `${radius * 2}px`,
+    }}
+    className={`absolute flex flex-col items-center justify-center bg-amber-600 border-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105`}
+  >
     <Icon className={`w-6 h-6 md:w-10 md:h-10 text-white`} />
     <div className="text-xs md:text-sm mt-1 text-white font-bold drop-shadow-md">{label}</div>
   </a>
