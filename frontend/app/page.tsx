456abd3df8952de35c150521ecb05a5fe6837cfc
ctxx3 <woojtekminecraft@gmail.com>
2024-12-14 16:02:16 +0100

    [feature] Add navigation component and update main page layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,18 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from 'next/link';
+import Navigation from "./ui/mainpage/navigation";
 
 
 export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 ">
-      <main className="flex flex-col row-start-2 items-center">
-        <Link href={'/activity'}>
-        <button
-          className="shadow-[inset_0_0_0_2px_#616467] text-black px-12 py-4 rounded-full tracking-widest 
-        uppercase font-bold bg-transparent hover:bg-[#616467] hover:text-white dark:text-neutral-200 
-        transition duration-200"
-        >
-          {" "}
-          Let's LiveActive!{" "}
-        </button>
-        </Link>
+    <div className="flex min-h-screen p-8 relative w-full justify-center">
+      <main className="flex flex-col items-center">
+        <h1 className="inline-block px-6 py-2 mt-12 mb-6 bg-amber-600 border-2 border-white rounded-full shadow-lg text-3xl text-white drop-shadow-md font-bold">
+          Golden Age
+        </h1>
+        <Navigation></Navigation>
       </main>
+      <a href="login" className="absolute bottom-4 start-1/2 -translate-x-1/2 px-4 py-1 bg-amber-600 border-2 border-white rounded-full shadow-lg text-md text-white drop-shadow-md hover:bg-amber-600 transition duration-300 ease-in-out transform hover:scale-105">
+          Jesteś wolontariuszem?
+        </a>
     </div>
   );
 }
