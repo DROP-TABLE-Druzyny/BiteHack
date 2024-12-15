fc53358da3a5ade2500c3c4e0a18e610ded15631
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-15 04:24:04 +0100

    Refactor: Clean up imports and improve SideNav component structure

--- frontend/app/ui/sidenav-events.tsx
+++ frontend/app/ui/sidenav-events.tsx
@@ -1,8 +1,4 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/nav-links';
-import SomeLogo from '@/app/ui/some-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
-import SideNav from './sidenav';
+import SideNav from "./sidenav";
 
 export default function SideNavEvents({
   children,
@@ -10,9 +6,7 @@ export default function SideNavEvents({
   children: React.ReactNode;
 }) {
   return (
-    
     <SideNav>
-      {/* flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 */}
       <div className="flex flex-col p-2 md:px-2 gap-4 h-full w-full">
         {children}
       </div>
