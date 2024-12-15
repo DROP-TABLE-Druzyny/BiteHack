6394d311a337a36c80b3f749df56b401dfd92ab9
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-15 05:23:45 +0100

    Fixed logo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,14 @@
+import UsersIcon from "@heroicons/react/24/outline/UsersIcon";
 import Navigation from "./ui/mainpage/navigation";
-
+import { lusitana } from '@/app/ui/fonts';
 
 export default function Page() {
   return (
     <div className="flex min-h-screen p-8 relative w-full justify-center">
       <main className="flex flex-col items-center">
-        <h1 className="inline-block px-6 py-2 mt-12 mb-6 bg-amber-600 border-2 border-white rounded-full shadow-lg text-3xl text-white drop-shadow-md font-bold">
-          Golden Age
+        <h1 className="flex flex-row px-8 py-3 mt-12 mb-6 bg-amber-600 border-2 border-white rounded-full shadow-lg text-4xl text-white drop-shadow-md font-bold ">
+         <UsersIcon className="h-10 w-10 mr-2" />
+          <p className={lusitana.className}>eSenior</p>
         </h1>
         <Navigation/>
       </main>
