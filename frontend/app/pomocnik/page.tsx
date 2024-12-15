d3fa2dc6ea519a1e93378c97ea0590914f6a05fc
ctxx3 <woojtekminecraft@gmail.com>
2024-12-15 07:02:34 +0100

    [feature] implement RequestItem component and update HelpRequest page

--- frontend/app/pomocnik/page.tsx
+++ frontend/app/pomocnik/page.tsx
@@ -4,6 +4,10 @@ import React, { useEffect, useState } from "react";
 import Header from "@/app/ui/login/header";
 import { publicService } from "@/services";
 import { HelpRequest } from "@/services/HelpRequest";
+import RequestItem from "../ui/pomocnik/prosba/request-item";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { formatDistanceToNow } from "date-fns";
+import { pl } from 'date-fns/locale'
 
 const Page = () => {
     const [items, setItems] = useState<HelpRequest[]>([]);
@@ -12,6 +16,7 @@ const Page = () => {
         const fetchItems = async () => {
             try {
                 const response = await publicService.getHelpRequests({});
+                console.log("Items fetched", response); // Log the response
                 setItems(response); // Adjust based on actual response structure
             } catch (error) {
                 console.error("Error fetching items:", error);
@@ -22,26 +27,22 @@ const Page = () => {
     }, []);
 
     return (
-        <div className="flex min-h-screen p-8 w-full justify-center">
+        <div className="flex min-h-screen p-8 w-full justify-center w-100">
             <main className="flex flex-col items-center">
                 <Header text="Moje pomoce" backUrl="/" />
 
-                <div className="flex flex-col items-center mt-2">
-                    <div>
+                <div className="flex flex-col items-center mt-2 w-full">
                         {items.length > 0 ? (
                             items.map((item, index) => (
-                                <div key={index}>
-                                    {item.type}
-                                </div>
+                                <RequestItem key={index} category="Nieokreślona" categoryIcon={ShoppingCartIcon} timeLeft={"Pozostało: " + formatDistanceToNow(item.expiration, {locale: pl})} location="Kraków Arena" status="pending" />
                             ))
                         ) : (
                             <p>No items found</p>
                         )}
                     </div>
-                    <a href="/pomocnik/prosba" className="bg-amber-700 border-2 text-white border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 mt-4 text-3xl font-semibold px-8 py-4">
+                    <a href="/pomocnik/prosba" className="bg-amber-600 border-2 text-white border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 mt-4 text-3xl font-semibold px-8 py-4">
                         Nowa prośba
                     </a>
-                </div>
             </main>
         </div>
     );
