eeba439160c226fd543b51582195b126e7095d23
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-15 02:15:35 +0100

    feat: enhance Help Request form with time selection and improved tile handling

--- frontend/app/pomocnik/prosba/page.tsx
+++ frontend/app/pomocnik/prosba/page.tsx
@@ -15,7 +15,8 @@ import {
 } from "@heroicons/react/24/outline";
 
 import TimeDropdown from "@/app/ui/pomocnik/prosba/time-dropdown";
-import { useState } from 'react';
+import React, { useState } from "react";
+import clsx from "clsx";
 
 export type HelpRequestTypes =
   | "SHOPPING"
@@ -25,12 +26,13 @@ export type HelpRequestTypes =
   | "WALK"
   | "OTHER";
 
-export default function Page() {
+const Page = () => {
   const [selectedTile, setSelectedTile] = useState<string | null>(null);
+  const [timeDropdownValue, setTimeDropdownValue] = useState(null);
 
   const tilesData = [
     { title: "Zakupy", icon: ShoppingCartIcon },
-    { title: "Medyczne", icon: ShieldCheckIcon },
+    { title: "Medyczna", icon: ShieldCheckIcon },
     { title: "Transport", icon: TruckIcon },
     { title: "Opieka", icon: HeartIcon },
     { title: "Spacer", icon: UsersIcon },
@@ -42,35 +44,61 @@ export default function Page() {
     setSelectedTile(title);
   };
 
+  const handleTimeDropdownChange = (value:any) => {
+    setTimeDropdownValue(value);
+  };
+
   return (
     <div className="flex min-h-screen p-8 w-full justify-center">
       <main className="flex flex-col items-center">
-          <Header text="Prośba" backUrl="/" />
+        <Header text="Prośba" backUrl="/" />
 
-            <form className="flex flex-col items-center mt-2">
+        <form className="flex flex-col items-center mt-2">
+          <div className="grid grid-cols-3 md:grid-cols-3 gap-4 mt-8">
+            {tilesData.map((tileData, idx) => (
+              <RequestTypeTile
+                key={idx}
+                tileData={tileData}
+                isSelected={selectedTile === tileData.title}
+                onClick={() => handleTileClick(tileData.title)}
+              />
+            ))}
+          </div>
 
-            <div className="grid grid-cols-3 md:grid-cols-3 gap-4 mt-8">
-              {tilesData.map((tileData, idx) => (
-                
-                <RequestTypeTile
-                  key={idx}
-                  tileData={tileData}
-                  isSelected={selectedTile === tileData.title}
-                  onClick={() => handleTileClick(tileData.title)}
-                />
+          <div
+            className={clsx("mt-16 transition-opacity duration-500", {
+              "opacity-100": selectedTile !== null,
+              "opacity-0": selectedTile === null,
+            })}
+          >
+            <TimeDropdown onChange={handleTimeDropdownChange} />
+          </div>
 
-              ))}
-            </div>
+          <Button
+            label="Lokalizuj"
+            className={clsx(
+              "mt-20 text-3xl font-semibold px-12 py-4 transition-opacity duration-500",
+              {
+                "opacity-100": timeDropdownValue !== null,
+                "opacity-0": timeDropdownValue === null,
+              }
+            )}
+          />
 
-            <div className="mt-16">
-              <TimeDropdown />
-            </div>
-
-            <Button label="Lokalizuj" className="mt-4 text-3xl font-semibold px-12 py-4" />
-          
-            <Button label="Dodaj prośbę" className="mt-4 text-3xl font-semibold px-8 py-4" />
+          <Button
+            label="Dodaj prośbę"
+            className={clsx(
+              "mt-4 text-3xl font-semibold px-8 py-4 transition-opacity duration-1000",
+              {
+                "opacity-100": timeDropdownValue !== null,
+                "opacity-0": timeDropdownValue === null,
+              }
+            )}
+          />
         </form>
       </main>
     </div>
   );
-}
+};
+
+export default Page;
--- frontend/app/ui/pomocnik/prosba/time-dropdown.tsx
+++ frontend/app/ui/pomocnik/prosba/time-dropdown.tsx
@@ -6,27 +6,32 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export default function TimeDropdown() {
+export default function TimeDropdown({ onChange } :{onChange: (value: string) => void}) {
+  const handleItemClick = (value:string) => {
+    onChange(value);
+  };
+
   return (
+
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="default" className="bg-amber-600 border-2 px-4 py-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 text-2xl p-8">Jak długo mogę czekać</Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-72 p-2">
 
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleItemClick("1hour")}>
           Do 1 godziny
         </DropdownMenuItem>
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleItemClick("2hour")}>
           Do 2 godzin
         </DropdownMenuItem>
-        <DropdownMenuItem>
-          Do 8 godzin
+        <DropdownMenuItem onClick={() => handleItemClick("4hour")}>
+          Do 4 godzin
         </DropdownMenuItem>
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleItemClick("1day")}>
           Do 1 dnia
         </DropdownMenuItem>
-        <DropdownMenuItem>
+        <DropdownMenuItem onClick={() => handleItemClick("1week")}>
           Do 1 tygodnia
         </DropdownMenuItem>
 
