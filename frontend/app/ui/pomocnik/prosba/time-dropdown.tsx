2341d0293daa403c44f60fa4dd28aadf7c63f971
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-12-15 02:49:03 +0100

    feat: enhance TimeDropdown component with dynamic options and selected value display

--- frontend/app/ui/pomocnik/prosba/time-dropdown.tsx
+++ frontend/app/ui/pomocnik/prosba/time-dropdown.tsx
@@ -1,41 +1,43 @@
-import { Button } from "@/components/ui/button"
+'use client';
+import React, { useState } from 'react';
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+
+const options: { [key: string]: string } = {
+  "1": "Do 1 godziny",
+  "2": "Do 2 godzin",
+  "4": "Do 4 godzin",
+  "24": "Do 1 dnia",
+  "168": "Do 1 tygodnia"
+};
 
 export default function TimeDropdown({ onChange } :{onChange: (value: string) => void}) {
-  const handleItemClick = (value:string) => {
+  const [selectedValue, setSelectedValue] = useState<keyof typeof options | null>(null);
+
+  const handleItemClick = (value: string) => {
+    setSelectedValue(value);
     onChange(value);
   };
 
   return (
-
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="default" className="bg-amber-600 border-2 px-4 py-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 text-2xl p-8">Jak długo mogę czekać</Button>
+        <Button variant="default" className="bg-amber-600 border-2 px-4 py-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105 text-2xl p-8">
+          {selectedValue ? `Wybrano: ${options[selectedValue]}` : 'Jak długo mogę czekać'}
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-72 p-2">
-
-        <DropdownMenuItem onClick={() => handleItemClick("1hour")}>
-          Do 1 godziny
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleItemClick("2hour")}>
-          Do 2 godzin
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleItemClick("4hour")}>
-          Do 4 godzin
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleItemClick("1day")}>
-          Do 1 dnia
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => handleItemClick("1week")}>
-          Do 1 tygodnia
-        </DropdownMenuItem>
-
+        {Object.entries(options).map(([value, label]) => (
+          <DropdownMenuItem key={value} onClick={() => handleItemClick(value)}>
+            {label}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
