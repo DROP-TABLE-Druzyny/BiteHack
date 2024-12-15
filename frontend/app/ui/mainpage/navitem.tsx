1544d644d5b20b8997ae68a7f98999fc2ac57868
MrNtex <rtrniemiec@gmail.com>
2024-12-15 08:47:29 +0100

    feat: Extended the hero page to add aditional links

--- frontend/app/ui/mainpage/navitem.tsx
+++ frontend/app/ui/mainpage/navitem.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavItemProps } from './navigation';
 
 
-const NavItem: React.FC<NavItemProps> = ({ href, icon: Icon, label, radius, style}) => (
+const NavItem: React.FC<NavItemProps> = ({ href, icon: Icon, label, radius, style, bgcolor}) => (
   <a
     href={href}
     style={{
@@ -10,7 +10,7 @@ const NavItem: React.FC<NavItemProps> = ({ href, icon: Icon, label, radius, styl
       width: `${radius * 2}px`, // Double the radius for width and height
       height: `${radius * 2}px`,
     }}
-    className={`absolute flex flex-col items-center justify-center bg-amber-600 border-2 border-white rounded-full shadow-lg hover:bg-amber-700 transition duration-300 ease-in-out transform hover:scale-105`}
+    className={`${bgcolor} absolute flex flex-col items-center justify-center border-2 border-white rounded-full shadow-lg transition duration-300 ease-in-out transform hover:scale-105`}
   >
     <Icon className={`w-12 h-12  md:w-10 md:h-10 text-white`} />
     <div className="text-md md:text-lg mt-1 text-white font-bold drop-shadow-md">{label}</div>
